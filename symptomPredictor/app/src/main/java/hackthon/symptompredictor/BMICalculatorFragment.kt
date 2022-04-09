af3e44b4fa26efdb56fc80762a19dfa88e1eff7f
Pacy Wu <peihsi5@uw.edu>
2022-04-09 13:42:27 -0700

    modify fragment name

--- symptomPredictor/app/src/main/java/hackthon/symptompredictor/BMICalculatorFragment.kt
+++ symptomPredictor/app/src/main/java/hackthon/symptompredictor/BMICalculatorFragment.kt
@@ -6,14 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import hackthon.symptompredictor.databinding.FragmentSecondBinding
+import hackthon.symptompredictor.databinding.FragmentBmiCalculatorBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class BMICalculatorFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+    private var _binding: FragmentBmiCalculatorBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +24,7 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = FragmentBmiCalculatorBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -33,7 +33,7 @@ class SecondFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+            findNavController().navigate(R.id.action_BMICalculatorFragment_to_SymptomCheckerFragment)
         }
     }
 
--- symptomPredictor/app/src/main/java/hackthon/symptompredictor/SymptomCheckerFragment.kt
+++ symptomPredictor/app/src/main/java/hackthon/symptompredictor/SymptomCheckerFragment.kt
@@ -6,14 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import hackthon.symptompredictor.databinding.FragmentFirstBinding
+import hackthon.symptompredictor.databinding.FragmentSymptomCheckerBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class SymptomCheckerFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentSymptomCheckerBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +24,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentSymptomCheckerBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -33,7 +33,7 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            findNavController().navigate(R.id.action_SymptomCheckerFragment_to_BMICalculatorFragment)
         }
     }
 
